7fb37c9a3c47616c7904cbe0c72e7393576cb91e
Kevin <vargaskevv@gmail.com>
2023-06-25 17:01:58 -0400

    Add activity item

---
+++ lib/db/activity_db.dart
@@ -0,0 +1,23 @@
+import 'package:isar/isar.dart';
+import 'package:temea/utils/utils.dart';
+
+part 'activity_db.g.dart';
+
+@Collection(accessor: "activity")
+@Name("activity")
+class ActivityDb {
+  @Index(unique: true)
+  late String id;
+
+  Id get isarId => fastHash(id);
+
+  @Index()
+  late String name;
+
+  @Index()
+  late DateTime createdAt;
+
+  DateTime? startedAt;
+
+  DateTime? deletedAt;
+}
