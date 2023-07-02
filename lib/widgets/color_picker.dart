9ae5f65451973bd0a3b05a6809e85ecb1b9eff44
Kevin <vargaskevv@gmail.com>
2023-07-01 20:12:40 -0400

    Color picker dialog

--- lib/widgets/color_picker.dart
+++ lib/widgets/color_picker.dart
@@ -11,11 +11,53 @@ class ColorPicker extends StatelessWidget {
   final CategoryColor color;
   final Function onSelect;
 
+  void _closeDialog(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  void _pickColor(BuildContext context, CategoryColor color) {
+    onSelect(color);
+    _closeDialog(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Pick a color'),
-      content: const Text('Not implemented yet!'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            height: 180,
+            width: 200,
+            child: GridView.count(
+              crossAxisCount: 4,
+              // padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 1.6,
+              children: CategoryColor.values
+                  .map((catCol) => InkWell(
+                        onTap: () => _pickColor(context, catCol),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: getCatColor(color: catCol, context: context),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(2)),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => _closeDialog(context),
+          child: const Text('Cancel'),
+        )
+      ],
     );
   }
 }
