6a54d5a252c9b2c0acf74f5a6802b164c564ba00
Kevin <vargaskevv@gmail.com>
2023-07-10 08:56:29 -0400

    Add default icon name

--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'icons/material.dart';
 
+const defaultIconName = 'square_rounded';
+
 IconData getMaterialIconData(String name) {
   if (materialIconMap.containsKey(name)) {
     return materialIconMap[name]!;
   }
-  return materialIconMap['square_rounded']!;
+  return materialIconMap[defaultIconName]!;
 }
